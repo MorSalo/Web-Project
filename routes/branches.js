006ff4fdab8e565a584d0fbf50f33ac68fb64d50
MorSalo <mor.sl2155@gmail.com>
2023-03-29 03:28:49 +0300

    29.3 3:28

--- routes/branches.js
+++ routes/branches.js
@@ -5,10 +5,11 @@ const controllerBranches = require('../controllers/branches');
 router.route('/')
     .get(controllerBranches.getBranches)
     .post(controllerBranches.createBranch);
-
 router.route('/:id')
     .get(controllerBranches.getBranchById)
     .put(controllerBranches.updateBranch)
     .delete(controllerBranches.deleteBranch);
+router.route('/:city?/:years?/:open?')
+    .get(controllerBranches.findBranch);
 
 module.exports = router;
