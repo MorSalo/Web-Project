e940980b722661ec170a9061400f38c39063899f
TheArbelim11 <123243741+TheArbelim11@users.noreply.github.com>
2023-03-30 23:52:03 +0300

    Add files via upload

--- songs.js
+++ songs.js
@@ -1,98 +1,33 @@
-const Songs = require('../models/songs')
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const getSongs = async (req, res) => {
-    const songs = await Songs.find()
-
-    res.json({
-        songs
-    })
-};
-
-const createSong = async (req, res) => {
-    const {name, author, rating, haveVideo, link} = req.body
-    await validateSongRequest(req, res)
-    const newSong = new Songs({
-        name,
-        author,
-        rating,
-        haveVideo,
-        link
-    })
-
-    await newSong.save().catch(e => console.log(e))
-    const io = req.app.get('socketio');
-    io.emit('new-song', {
-        song: newSong
-    });
-    
-    res.json({
-        newSong
-    })
-}
-
-function IsEmpty(name, author, rating) {
-    return (!name || !author || !rating);
-
-}
-
-async function validateSongRequest(req, res) {
-    const {name, author, rating, haveVideo, link} = req.body;
-    if (IsEmpty(name, author, rating)) {
-        return res.status(404).json({errors: ['Missing some variables exists']});
-
+const Songs = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    rating: {
+        type: Number,
+        required: true
+    },
+    haveVideo: {
+        type: Boolean,
+        required: true
+    },
+    link: {
+        type: String,
+        required: false,
+        default:''
+    },
+    published: {
+        type: Date,
+        required: false,
+        default: new Date()
     }
-    const dbsong = await Songs.findOne({name, author, rating, haveVideo, link});
+});
 
-    if (dbsong) {
-        return res.status(404).json({errors: ['Song already exists']});
-    }
-
-}
-
-const updateSong = async (req, res) => {
-    const {id} = req.params
-    const {name, author, rating, haveVideo, link} = req.body
-    await validateSongRequest(req, res)
-
-    const song = await Songs.findById(id)
-
-    if (!song) {
-        return res.status(404).json({errors: ['Song not found']});
-    }
-
-    await Songs.updateOne({_id: id}, {name, author, rating, haveVideo, link})
-
-    const songs = await Songs.find()
-    res.json({
-        songs,
-        message: 'Song updated!'
-    })
-}
-
-const deleteSong = async (req, res) => {
-    const {id} = req.params
-
-    const deleted_song = await Songs.findById(id)
-
-    if (!deleted_song) {
-        return res.status(404).json({errors: ['Song not found']});
-    }
-
-    await Songs.deleteOne({_id: id})
-    res.send('Song deleted!');
-    const io = req.app.get('socketio');
-    io.emit('deleted-song', {
-        song: deleted_song
-    });
-    const songs = await Songs.find()
-    res.json({
-        songs
-    })
-}
-
-module.exports = {
-    getSongs,
-    createSong,
-    updateSong,
-    deleteSong
-};
+module.exports = mongoose.model('Songs', Songs);
