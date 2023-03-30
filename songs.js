cf49cafc45c3660bbdc7cbd9ded01c9abba73578
TheArbelim11 <123243741+TheArbelim11@users.noreply.github.com>
2023-03-30 23:50:41 +0300

    Add files via upload

--- songs.js
+++ songs.js
@@ -1,13 +1,98 @@
-const express = require('express');
-const router = express.Router();
-const songsController = require('../controllers/songs');
+const Songs = require('../models/songs')
 
-router.route('/')
-    .get(songsController.getSongs)
-    .post(songsController.createSong)
+const getSongs = async (req, res) => {
+    const songs = await Songs.find()
 
-router.route('/:id')
-    .put(songsController.updateSong)
-    .delete(songsController.deleteSong)
+    res.json({
+        songs
+    })
+};
 
-module.exports = router;
+const createSong = async (req, res) => {
+    const {name, author, rating, haveVideo, link} = req.body
+    await validateSongRequest(req, res)
+    const newSong = new Songs({
+        name,
+        author,
+        rating,
+        haveVideo,
+        link
+    })
+
+    await newSong.save().catch(e => console.log(e))
+    const io = req.app.get('socketio');
+    io.emit('new-song', {
+        song: newSong
+    });
+    
+    res.json({
+        newSong
+    })
+}
+
+function IsEmpty(name, author, rating) {
+    return (!name || !author || !rating);
+
+}
+
+async function validateSongRequest(req, res) {
+    const {name, author, rating, haveVideo, link} = req.body;
+    if (IsEmpty(name, author, rating)) {
+        return res.status(404).json({errors: ['Missing some variables exists']});
+
+    }
+    const dbsong = await Songs.findOne({name, author, rating, haveVideo, link});
+
+    if (dbsong) {
+        return res.status(404).json({errors: ['Song already exists']});
+    }
+
+}
+
+const updateSong = async (req, res) => {
+    const {id} = req.params
+    const {name, author, rating, haveVideo, link} = req.body
+    await validateSongRequest(req, res)
+
+    const song = await Songs.findById(id)
+
+    if (!song) {
+        return res.status(404).json({errors: ['Song not found']});
+    }
+
+    await Songs.updateOne({_id: id}, {name, author, rating, haveVideo, link})
+
+    const songs = await Songs.find()
+    res.json({
+        songs,
+        message: 'Song updated!'
+    })
+}
+
+const deleteSong = async (req, res) => {
+    const {id} = req.params
+
+    const deleted_song = await Songs.findById(id)
+
+    if (!deleted_song) {
+        return res.status(404).json({errors: ['Song not found']});
+    }
+
+    await Songs.deleteOne({_id: id})
+    res.send('Song deleted!');
+    const io = req.app.get('socketio');
+    io.emit('deleted-song', {
+        song: deleted_song
+    });
+    const songs = await Songs.find()
+    res.json({
+        songs
+    })
+}
+
+module.exports = {
+    getSongs,
+    createSong,
+    updateSong,
+    deleteSong
+};
--- users.js
+++ users.js
@@ -1,17 +1,133 @@
-const express = require('express');
-const router = express.Router();
-const usersController = require('../controllers/users');
+const Users = require('../models/users')
+const jwt = require('jsonwebtoken');
+const getUsers = async (req, res) => {
+    const users  = await Users.find()
 
-router.route('/')
-    .get(usersController.getUsers)
-    .post(usersController.createUser)
+    res.json({
+        users
+    })
+};
+function IsEmpty(username,email,password)
+{
+    return (!username || !email || !password);
+        
+}
 
-router.route('/:id')
-    .get(usersController.getUser)
-    .put(usersController.updateUser)
-    .delete(usersController.deleteUser)
+async function validateUserRequest(req,res){
+    const {username, email, password} = req.body;
+    if(IsEmpty(username, email, password))
+    {
+        return res.status(404).json({errors: ['Missing some variables exists']});
 
-router.route('/auth')
-.post(usersController.validateUser)
+    }
+    const dbuser = await Users.findOne({ $or: [{ username }, { email }] });
+    
+    if(dbuser){
+        return res.status(404).json({errors: ['User already exists']});
+    }
 
-module.exports = router;
+}
+const createUser = async (req, res) => {
+    const {username, email, password, isAdmin} = req.body
+    await validateUserRequest(req,res)
+    const newUser = new Users({
+        username,
+        email,
+        password,
+        isAdmin
+    })
+
+    await newUser.save().catch(e => console.log(e))
+    res.json({
+        newUser,
+        message:'New user created!'
+    })
+}
+
+const updateUser = async (req, res) => {
+    const {id} = req.params
+    const {username, email, password,isAdmin} = req.body
+    await validateUserRequest(req,res)
+
+    const user = await Users.findById(id)
+
+    if (!user) {
+        return res.status(404).json({errors: ['User not found']});
+    }
+
+    await Users.updateOne({_id:id},{username, email, password,isAdmin})
+
+    const users = await Users.find()
+    res.json({
+        users,
+        message:'Song updated!'
+    })
+}
+
+const deleteUser = async (req, res) => {
+    const {id} = req.params
+
+    const user = await Users.findById(id)
+
+    if (!user) {
+        return res.status(404).json({errors: ['User not found']});
+    }
+
+    await Users.deleteOne({_id: id})
+
+    const users = await Songs.find()
+    res.json({
+        users,
+        message: 'User deleted!'
+
+    })
+}
+
+const validateUser = async (req, res) => {
+    const {email, password} = req.body
+    if(!email || !password)
+    {
+        return res.status(404).json({errors: ['Missing some variables exists']});
+
+    }
+    const user = await Users.findOne({email, password});
+    if(!user)
+    {
+        return res.status(404).json({errors: ['Incorrect email or password.']});
+
+    }
+    const token = jwt.sign({ sub: user.id }, process.env.JWT_SECRET_KEY);
+    res.json({ token });
+  };
+
+  const getUser = async (req,res) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+  
+    if (token == null) {
+      return res.sendStatus(401);
+    }
+  
+    jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, user) => {
+      if (err) {
+        return res.sendStatus(403);
+      }
+      const dbUser = await Users.findOne({_id: user.sub});
+
+      if (dbUser) {
+        res.json({ user:dbUser });
+      } else {
+        res.status(404).json({ message: 'User not found' });
+      }
+    });
+
+  }
+
+module.exports = {
+    getUser,
+    getUsers,
+    createUser,
+    updateUser,
+    deleteUser,
+    validateUser
+};
