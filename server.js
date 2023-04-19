75bf231948054eed1bcb2c0f146560bf2761d56e
Alond44222 <alond135345637@gmail.com>
2023-04-19 20:48:47 +0300

    Working commit

--- server.js
+++ server.js
@@ -15,13 +15,13 @@ const server = http.createServer(app);
 const io = require("socket.io")(server,{cors: {
     origin: "*",
   }});
-var Twitter = require('twitter');
-const Twitterclient = new Twitter({
-  consumer_key: 'u0Z9fky4EN0piQ3bwJWlCtlCa',
-  consumer_secret: 'xPXzSY2wOudm4dpRDLUz9CO3CMTN2vD0CjDmodCBATV9dKytS5',
-  access_token_key: '1648417416337055745-FQpWqnRJ75tO1f7MtsUFnaVSLQt8zp',
-  access_token_secret: 'I8T00IDw9QO4c8LSQaCcbNjE9dH2gSsAKPRlEzZAItisI'
-});
+
+  //////////////////////// Code Alon added to allow fetch /////////////
+  app.use(cors({
+    origin: 'https://localhost:3000',
+  'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  }));
+  //////////////////////////////////////////
 
 mongoose.connect(process.env.MongodbConectionString)
   .then(() => console.log('Connected!'));
@@ -55,6 +55,35 @@ app.use('/songs', songs);
 app.use('/users' ,users);
 app.use('/branches' ,branches);
 app.set('socketio', io);
+
+/////////////////////////////////////// Twitter Part /////////////////////////////////
+const Twitter = require('twitter');
+
+
+const client = new Twitter({
+  consumer_key: 'u0Z9fky4EN0piQ3bwJWlCtlCa',
+  consumer_secret: 'xPXzSY2wOudm4dpRDLUz9CO3CMTN2vD0CjDmodCBATV9dKytS5',
+  access_token_key: '1648417416337055745-FQpWqnRJ75tO1f7MtsUFnaVSLQt8zp',
+  access_token_secret: 'I8T00IDw9QO4c8LSQaCcbNjE9dH2gSsAKPRlEzZAItisI'
+});
+
+
+app.post('/tweet', (req, res) => {
+  console.log("got to post");
+  const tweet = req.body.tweet;
+
+  client.post('statuses/update', {status: tweet}, (error, tweet, response) => {
+    if (error) {
+      console.log(error);
+      res.sendStatus(500);
+    } else {
+      console.log(tweet);
+      res.sendStatus(200);
+    }
+  });
+});
+//////////////////////////
+
 server.listen(3000, function () {
     console.log('listening on 3000')
 })
